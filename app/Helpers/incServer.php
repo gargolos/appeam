d444de1c57d99ffd46f24b6fbf963d0f1558a6a1
gargolos <gargolos@gmail.com>
2021-04-17 10:28:14 -0500

    Middleware Auth 1.2

--- app/Helpers/incServer.php
+++ app/Helpers/incServer.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/../../vendor/firebase/jwt/src/BeforeValidException.php';
 require_once __DIR__ . '/../../vendor/firebase/jwt/src/ExpiredException.php';
 require_once __DIR__ . '/../../vendor/firebase/jwt/src/SignatureInvalidException.php';
-require_once __DIR__ . '/../../vendor/firebase/jwt/src/JWT.php'; 
-
+require_once __DIR__ . '/../../vendor/firebase/jwt/src/JWT.php';  
+    
 ?>
